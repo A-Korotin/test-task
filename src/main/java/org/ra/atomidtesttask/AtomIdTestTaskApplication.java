46388da7fa7972da900f315e0aaffb1fdde2eec2
Alexey Korotin <sx12fx@gmail.com>
2024-03-22 21:31:27 +0300

    Initial commit

---
+++ src/main/java/org/ra/atomidtesttask/AtomIdTestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.ra.atomidtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomIdTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtomIdTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ra/atomidtesttask/AtomIdTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ra.atomidtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomIdTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
